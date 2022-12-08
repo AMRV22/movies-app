b238f22b5925b01e582c589cb54aee647ae98c94
Ana Rodriguez <tec.ana19@gmail.com>
2022-12-07 20:19:54 -0600

    feature: add styled components to login screen

--- src/pages/loginScreen/LoginScreen.tsx
+++ src/pages/loginScreen/LoginScreen.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import loginImage from "../../assets/images/cinema.svg";
 import Input from "../../components/styledComponents/elements/inputs";
+import * as S from "./styles";
 
 const LoginScreen = () => {
   return (
-    <div className='flex  h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8 '>
-      <div className='bg-white grid grid-cols-1 md:grid-cols-2  shadow sm:rounded-lg'>
-        <div className='md:w-full md:max-w-md space-y-8 m-12'>
+    <S.LoginContainer>
+      <S.LoginFormContainer>
+        <div className='md:max-w-md space-y-8 m-12'>
           <div>
-            <h2 className='mt-6 text-center text-3xl md:text-6xl font-bold tracking-tight text-secondary'>
+            <S.LoginTitle >
               MyMovies website
-            </h2>
+            </S.LoginTitle>
           </div>
           <form className='mt-8 space-y-6' action='#' method='POST'>
             <div className='space-y-4 rounded-md shadow-sm'>
@@ -25,36 +26,34 @@ const LoginScreen = () => {
               </div>
               <div>
                 <label className='sr-only'>Password</label>
-                <input
+                <Input
                   id='password'
                   name='password'
                   type='password'
                   required
-                  className='relative block w-full appearance-none rounded  border border-gray-300 px-3 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-secondary focus:outline-none focus:ring-secondary sm:text-sm'
                   placeholder='Password'
                 />
               </div>
             </div>
 
             <div>
-              <button
+              <S.LoginButton
                 type='submit'
-                className='group relative flex w-full h-10 justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-md font-medium text-white'
               >
                 Sign in
-              </button>
+              </S.LoginButton>
             </div>
           </form>
         </div>
-        <div className='md:w-full md:max-w-md py-5'>
+        <div className='md:max-w-md py-5'>
           <img
             className='mx-auto max-h-fit h-36 md:h-fit  w-auto'
             src={loginImage}
             alt='Login Image'
           />
         </div>
-      </div>
-    </div>
+      </S.LoginFormContainer>
+    </S.LoginContainer>
   );
 };
 
---
+++ src/pages/loginScreen/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const LoginTitle = styled.p`
+  ${tw`mt-6 text-center text-3xl md:text-6xl font-bold tracking-tight text-secondary`}
+`;
+
+const LoginButton = styled.button`
+${tw` relative flex w-full h-10 justify-center rounded-md border border-transparent bg-primary py-2 px-4  font-medium text-white`}
+`;
+
+const LoginContainer = styled.div`
+  ${tw`flex  h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8 `};
+`;
+
+const LoginFormContainer = styled.div`
+  ${tw`bg-white grid grid-cols-1 md:grid-cols-2 shadow sm:rounded-lg`}
+`;
+export { LoginTitle, LoginButton, LoginContainer, LoginFormContainer };
