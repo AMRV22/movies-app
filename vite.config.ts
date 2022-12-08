c594e784f19037bd72d9e0f38db14c60b8245bfd
AMRV22 <tec.ana19@gmail.com>
2022-12-07 19:14:10 -0600

    feature: add configuration for twin.macro support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-macros",
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
+            },
+          ],
+          [
+            "@babel/plugin-transform-react-jsx",
+            { pragma: "__cssprop" },
+            "twin.macro",
+          ],
+        ],
+      },
+    }),
+    svgr(),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
