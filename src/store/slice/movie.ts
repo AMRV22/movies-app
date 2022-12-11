7cb7ace68da769f2796fa3d732a148d0a9e2eb85
AMRV22 <tec.ana19@gmail.com>
2022-12-11 15:37:31 -0600

    feature: create movie reducer

---
+++ src/store/slice/movie.ts
@@ -0,0 +1,77 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  thunkFilterMoviesReducer,
+  thunkPopularMovieReducer,
+} from "./../reducers/movie.reducer";
+import IMovie from "utils/interfaces/IMovie";
+
+export type MovieState = {
+  page: number;
+  loading: boolean;
+  movies: IMovie[];
+  totalPages: number;
+  isFiltered: boolean;
+};
+
+const initialState: MovieState = {
+  page: 0,
+  loading: false,
+  movies: [],
+  totalPages: 0,
+  isFiltered: false,
+};
+
+export const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(thunkPopularMovieReducer.fulfilled, (_, action) => {
+        const { page, results, total_pages, isFiltered } = action.payload;
+        return {
+          page,
+          movies: results,
+          totalPages: total_pages,
+          loading: false,
+          isFiltered,
+        };
+      })
+      .addCase(thunkPopularMovieReducer.pending, (state) => {
+        return {
+          ...state,
+          loading: true,
+        };
+      })
+      .addCase(thunkPopularMovieReducer.rejected, () => {
+        return {
+          ...initialState,
+        };
+      });
+
+    builder
+      .addCase(thunkFilterMoviesReducer.fulfilled, (_, action) => {
+        const { page, results, total_pages, isFiltered } = action.payload;
+        return {
+          page,
+          movies: results,
+          totalPages: total_pages,
+          loading: true,
+          isFiltered,
+        };
+      })
+      .addCase(thunkFilterMoviesReducer.pending, (state) => {
+        return {
+          ...state,
+          loading: true,
+        };
+      })
+      .addCase(thunkFilterMoviesReducer.rejected, () => {
+        return {
+          ...initialState,
+        };
+      });
+  },
+});
+
+export default movieSlice.reducer;
